13a94f28cd76e386fc597f39fdd2220b038c0394
escarluka <mbamiluka@gmail.com>
2019-07-20 20:28:07 -0600

    Successfully check if parentheses are balanced. But, doesnt check adjacent operands yet.

--- MbamiLuka_05.java
+++ MbamiLuka_05.java
@@ -1,50 +1,106 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 /**
- * This class reads several lines of infix expressions from an input file, 
+ * This class reads several lines of infix expressions from an input file,
  * converting them into their respective postfix expressions by using the
  * convertToPostfix method from the Postfix class.
  * 
  * @author Mbami Luka + Zach Morlan
  *
  */
-public class MbamiLuka_05 
-{
+public class MbamiLuka_05 {
 
-	public static void main ( String[] args) throws FileNotFoundException 
-	{
+	public static void main ( String[] args) throws FileNotFoundException {
 		Postfix expression;
-		
-		final String INPUT_FILENAME;				// Name of inputFile
+
+		final String INPUT_FILENAME; // Name of inputFile
 		INPUT_FILENAME = "Project05_Input.txt";
-		
-		File inputFile = new File (INPUT_FILENAME);	// New input file 
-		Scanner input = new Scanner( inputFile);	// Scanner reading file
-		
-		String div = "_____________________________________________"
-				   + "_____________________________________________";
-		
+
+		File inputFile = new File ( INPUT_FILENAME); // New input file
+		Scanner input = new Scanner ( inputFile); // Scanner reading file
+
+		String div = "_____________________________________________" + 
+					 "_____________________________________________" ;
+
 		// Print Header and a divider
-		System.out.printf ("%-51s %30s\n", "INFIX", "POSTFIX");
-		System.out.println (div);
-		
-		
+		System.out.printf ( "%-51s %35s\n", "INFIX", "POSTFIX");
+		System.out.println ( div);
+
 		// Read each input line and convert to its Postfix expression
-		while ( input.hasNextLine())
+		while  ( input.hasNextLine ( )) 
 		{
-			String lineRead = input.nextLine();
-			String lineFormatted = lineRead.trim();
+			String lineRead = input.nextLine ( );
+
+			expression = new Postfix ( );
+			String exp;
+
+			if ( parenthesesIsBalanced ( lineRead)) 
+			{
+				exp = expression.convertToPostfix ( lineRead);
+				
+				if ( expression.operandCheck ( lineRead) == false)
+				{
+					exp = "None - Adjacent operands";
+				}
+			} 
+			else 
+			{
+				exp = "None - Uneven number of parentheses";
+			}
 			
-			expression = new Postfix ();
-			String exp = expression.convertToPostfix ( lineFormatted);
-			
+
 			System.out.printf ( "\n%-52s %37s\n", lineRead, exp);
-			System.out.println (div);
-			
-			
+			System.out.println ( div);
+
 		} // End while loop reading lines
-	input.close();
-	}
+
+		input.close ( );
+
+	} // End main
+
+	// *************************************************************************
+	/**
+     * Method checks to see if the number of parentheses 
+     * in the infix are the same amount
+     * @param	String infix
+     * @return	true if even amount of both parentheses
+     */
+	public static boolean parenthesesIsBalanced ( String infix) 
+	{
+		int left = 0;
+		int right = 0;
+
+		for  ( int i = 0; i < infix.length ( ); i++) 
+		{
+			if  ( infix.charAt ( i) == '(') 
+			{
+
+				left++;
+
+			} // End if
+			if  ( infix.charAt ( i) == ')') 
+			{
+
+				right++;
+
+			} // End if
+		} // End for loop
+		if  ( left == right) 
+		{
+
+			return true;
+
+		} // End if
+		else 
+		{
+
+			return false;
+
+		} // End else
+	} // End parenthesesCheck
+	
+	//*****************************************************************************
 
 }
--- Postfix.java
+++ Postfix.java
@@ -6,7 +6,7 @@
  *
  */
 public class Postfix 
-{	
+{
 		/**
 		 * This method checks if a specified character is any of the valid 
 		 * BIDMAS operators. 
@@ -27,8 +27,17 @@ public class Postfix
 	        
 	        return false;
 	    }
-
-	    public int getPrecedence ( char character) 
+	    //*********************************************************************
+	    /**
+	     * This method returns a value for the specific 
+	     * PEMDAS operators. 
+	     * @param	   character
+	     * @return     1 if the value is + or -
+	     * @return	   2 if the value is * or /
+         * @return	   3 if the value is ^
+	     * @return    -1 if the value is invalid
+	    */
+	    public int getPrecedence ( char character)
 	    {
 	        if ( character == '+' || character == '-') 
 	        {        
@@ -44,10 +53,13 @@ public class Postfix
 	            return 3;
 	        }
 	        return -1;
-	    }
+	        
+	    } // End getPrecedence
+	    
+	  //***********************************************************************
 
 	    // Check if the specified character is an operand
-	    public boolean isOperand ( char character) 
+	    public static boolean isOperand ( char character) 
 	    {
 	        if (character >= 'a' && character <= 'z' || 
 	        	character >= 'A' && character <= 'Z'
@@ -57,7 +69,24 @@ public class Postfix
 	        }
 	        return false;
 	    }
+	  //***********************************************************************
+	    public static boolean operandCheck ( String infix) 
+	    {
+	    	infix = infix.trim();
+		     char first;
+	         char second;
 
+	         for(int i = 0; i < infix.length() - 1; i++) {
+	            first = infix.charAt(i);
+	            second = infix.charAt(i+1);
+
+	            if(isOperand(first) && isOperand(second)){
+	               return false;
+	            }// End if             
+	         }// End for loop 
+	         return true;
+		   } // End operandCheck
+	  //***********************************************************************
 	    public String convertToPostfix ( String infix) 
 	    {
 	        Stack<Character> stack = new Stack<Character>();
@@ -66,20 +95,26 @@ public class Postfix
 	        char character;
 
 	        for ( int i = 0; i < infix.length(); i++) 
-	        {
+	        {	        	
+	        	infix = infix.trim();
+	        	
 	        	// pick each letter in infix string
-	            character = infix.charAt(i);
-
+	            character = infix.charAt (i);
+	            		
 	            // Then, check if character is an operand
 	            // If so, it will be added to the postfix variable
 	            if ( isOperand ( character)) 
 	            {
-	                postfix += ( character);
-	            } 
+	                postfix += ( character) + " ";
+	                	
+	            }
+	            
+	            // if we encounter '(' we push to stack
 	            else if ( character == '(') 
 	            {
 	                stack.push ( character);
 	            }
+	            
 	            // If current character is ‘)’, pop and output from stack
 	            // when ‘(‘ is encountered we stop popping.
 	            else if ( character == ')') 
@@ -87,7 +122,7 @@ public class Postfix
 
 	                while ( !stack.isEmpty() && stack.peek() != '(') 
 	                {
-	                	postfix += stack.pop();
+	                	postfix += stack.pop() + " ";
 	                }
 	                if ( !stack.isEmpty() && stack.peek() != '(')
 	                {
@@ -98,13 +133,14 @@ public class Postfix
 	                    stack.pop();
 	                }
 	            }
-	            else if 
-	            (isOperator ( character)) // operator encountered
+	            
+	            else if ( isOperator ( character))
 	            {
-	                if ( !stack.isEmpty() && 
-	                	  getPrecedence (character) <= getPrecedence ( stack.peek())) 
+	                if ( !stack.isEmpty () && 
+	                	  getPrecedence ( character) <= 
+	                	  getPrecedence ( stack.peek()	)) 
 	                {
-	                	postfix += ( stack.pop());
+	                	postfix += ( stack.pop() + " ");
 	                }
 	                stack.push ( character);
 	            }
@@ -112,8 +148,9 @@ public class Postfix
 
 	        while ( !stack.isEmpty()) 
 	        {
-	        	postfix += ( stack.pop());
+	        	postfix += ( stack.pop() + " ");
 	        }
 	        return postfix;
-	    }
+	        
+	    } // End convertToPostfix
 }
